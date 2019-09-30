aea0654bb702c4e7a2bdc6a01efc65e1067a1413
yatesab <yatesab@gmail.com>
2019-09-29 19:07:11 -0600

    added a different update command for plex

--- scripts/container/update.sh
+++ scripts/container/update.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-docker-compose -f $COMPOSEFILE pull $1
-docker-compose -f $COMPOSEFILE up -d $1
+if [ "$1" = "plex" ]
+then
+    docker restart plex > /dev/null
+    echo "Plex Updated"
+else
+    docker-compose -f $COMPOSEFILE pull $1
+    docker-compose -f $COMPOSEFILE up -d $1
+fi
